d7ccca9e92a470b4eb44417d871672e4486d2fd2
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-26 14:24:35 -0300

    feat(JPTextHeader):Create header module

---
+++ src/modules/JPTextHeader/JPTextHeader.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./JPTextHeader.styles.css";
+
+export const JPTextHeader = () => {
+  return (
+    <h1 className="header-title">
+      <span className="header-strong">JPT</span>ext
+    </h1>
+  );
+};
