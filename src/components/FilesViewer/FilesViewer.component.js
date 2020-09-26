7c97f3dd33e882922256883e13e21018feabee51
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-26 13:09:46 -0300

    refactor(FilesViewer.component.js):Implement css styles

--- src/components/FilesViewer/FilesViewer.component.js
+++ src/components/FilesViewer/FilesViewer.component.js
@@ -1,15 +1,28 @@
 import React from "react";
+import "./FilesViewer.styles.css";
 
 export const FilesViewer = ({ convertedText, file }) => {
   const hasFile = file instanceof File;
+  const imageSource = hasFile
+    ? URL.createObjectURL(file)
+    : "https://i.pinimg.com/originals/ae/8a/c2/ae8ac2fa217d23aadcc913989fcc34a2.png";
 
   return (
-    <div>
-      <textarea value={convertedText} onChange={() => {}} />
+    <div className="files-viewer--wrapper">
+      <textarea
+        className="files-viewer__text-input files-viewer__content"
+        value={convertedText}
+        onChange={() => {}}
+      />
 
-      {hasFile && (
-        <img key={file.name} src={URL.createObjectURL(file)} alt="preview" />
-      )}
+      <div className="files-viewer__preview--wrapper files-viewer__content">
+        <img
+          className="files-viewer__preview"
+          key={file.name}
+          src={imageSource}
+          alt="preview"
+        />
+      </div>
     </div>
   );
 };
