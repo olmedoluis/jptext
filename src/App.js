69d0e37a6abd3af800a01333586e9cc2ef071d37
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-26 00:57:36 -0300

    refactor(App.js):Read file uploaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { createWorker } from "tesseract.js";
 import "./App.css";
 import { FileUploader } from "./components/FileUploader/FileUploader.component";
@@ -7,7 +7,9 @@ function App() {
   const [status, setStatus] = useState("");
   const [convertedText, setConvertedText] = useState("");
 
-  const readImage = useCallback(async () => {
+  const readImage = useCallback(async (file) => {
+    setStatus("reading..");
+
     const worker = createWorker({
       logger: ({ status }) => setStatus(status),
     });
@@ -20,21 +22,17 @@ function App() {
 
     const {
       data: { text },
-    } = await worker.recognize(
-      "https://tesseract.projectnaptha.com/img/eng_bw.png"
-    );
+    } = await worker.recognize(file);
 
     setConvertedText(text);
     setStatus("Completed!");
   }, []);
 
-  useEffect(() => {
-    readImage();
-  }, [readImage]);
-
   return (
     <div className="App">
-      <FileUploader />
+      <FileUploader
+        onChange={(files) => files.forEach((file) => readImage(file))}
+      />
 
       <p>{convertedText}</p>
       <p>{status}</p>
