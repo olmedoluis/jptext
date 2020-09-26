5a20caf0864643ed1fe958170e534b5307ac31cc
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-26 19:54:12 -0300

    refactor(SelectInput.component.js):Show and hide options depending on button

--- src/components/SelectInput/SelectInput.component.js
+++ src/components/SelectInput/SelectInput.component.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Chevron } from "../../assets/icons/Chevron.icon";
+import "./SelectInput.styles.css";
 
 export const SelectInput = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const options = ["wea", "wea1"];
 
   return (
     <div className="select--wrapper">
-      <button className="select__button">
-        Select file <Chevron />
+      <button className="select__button" onClick={() => setIsOpen(state => !state)}>
+        <span>Select file</span> <Chevron />
       </button>
 
-      <div className="select__option--wrapper">
-        {options.map((option) => (
-          <div key={option} className="select__option">
-            {option}
-          </div>
-        ))}
-      </div>
+      {isOpen && (
+        <div className="select__option--wrapper">
+          {options.map((option) => (
+            <div key={option} className="select__option">
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
