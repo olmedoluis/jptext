8d2ba3531513550fad49dcd46873b1a8d5135083
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-26 00:56:11 -0300

    refactor(FileUploader.component.js):Expect for on change event as prop

--- src/components/FileUploader/FileUploader.component.js
+++ src/components/FileUploader/FileUploader.component.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export const FileUploader = () => {
-  return <input type="file" />;
+export const FileUploader = ({ onChange }) => {
+  const handleFileSelected = (event) => {
+    const files = Array.from(event.target.files);
+
+    onChange(files);
+  };
+
+  return <input type="file" onChange={handleFileSelected} />;
 };
