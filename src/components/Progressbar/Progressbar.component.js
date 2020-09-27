55470da5769b77ad8e30467eb422e9da65c506b4
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-27 17:29:48 -0300

    refactor(Progressbar.component.js):Implement styles

--- src/components/Progressbar/Progressbar.component.js
+++ src/components/Progressbar/Progressbar.component.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./Progressbar.styles.css";
 
-export const Progressbar = () => {
-  return <div>Progressbar</div>;
+export const Progressbar = ({ progress = 0 }) => {
+  const progressInverted = 100 - progress;
+
+  return (
+    <>
+      <div className="progressbar--wrapper">
+        <span className="progressbar">{progressInverted}%</span>
+        <span
+          className="progressbar__filler"
+          style={{ right: `${progressInverted}%` }}
+        />
+      </div>
+    </>
+  );
 };
