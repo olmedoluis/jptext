10fe5c81c39764a110a9820a96f7215db9d0b761
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-28 20:46:06 -0300

    refactor(jptext):Set up production environment

---
+++ build/precache-manifest.c79fe1fdfde8c11f8c0da23aa1bd7255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b931f44e60d7c4ca91935e400bd790a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c787f7230e385dd00a68",
+    "url": "/static/css/main.0eb304d0.chunk.css"
+  },
+  {
+    "revision": "7fa1812ea17441756292",
+    "url": "/static/js/2.a9613c9f.chunk.js"
+  },
+  {
+    "revision": "c787f7230e385dd00a68",
+    "url": "/static/js/main.27cc5929.chunk.js"
+  },
+  {
+    "revision": "53c1165e09655998f23b",
+    "url": "/static/js/runtime-main.b52cbde7.js"
+  }
+]);
--- src/modules/JPTextFooter/JPTextFooter.component.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./JPTextFooter.styles.css"
-
-export const JPTextFooter = () => {
-  return <div className="footer--wrapper"></div>;
-};
