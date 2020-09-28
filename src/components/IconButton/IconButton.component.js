1f82141c051d9cd7473490cac1c10a1eb023a8ea
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-27 23:19:33 -0300

    refactor(IconButton.component.js):Expect for className prop

--- src/components/IconButton/IconButton.component.js
+++ src/components/IconButton/IconButton.component.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./IconButton.styles.css"
+import "./IconButton.styles.css";
 
-export const IconButton = ({ text, icon, onClick }) => {
+export const IconButton = ({ text, icon, onClick, className }) => {
   return (
-    <button className="button" onClick={onClick}>
+    <button className={`button ${className}`} onClick={onClick}>
       <span>{text}</span>
       {icon}
     </button>
