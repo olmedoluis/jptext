f0aeddc418b6b7eff18d6fdb1c72d53656c84441
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-27 23:56:02 -0300

    refactor(IconButton.component.js):Implement tooltip

--- src/components/IconButton/IconButton.component.js
+++ src/components/IconButton/IconButton.component.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./IconButton.styles.css";
 
-export const IconButton = ({ text, icon, onClick, className }) => {
+export const IconButton = ({ text, icon, onClick, className, toolTipText }) => {
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+  const hasTooltip = Boolean(toolTipText);
+  toolTipText = "wea";
+
   return (
     <button className={`button ${className}`} onClick={onClick}>
+      {hasTooltip && (
+        <span className={`toolTip ${isTooltipOpen ? "open" : ""}`}>
+          {toolTipText}
+        </span>
+      )}
+
       <span>{text}</span>
+
       {icon}
     </button>
   );
