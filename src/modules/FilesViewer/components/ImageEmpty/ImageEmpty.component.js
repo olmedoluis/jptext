b8e81e7de2e2f9a758a668002df3734b7e2d9acb
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 18:24:00 -0300

    feat(ImageEmpty):Create component to show when there is no image

---
+++ src/modules/FilesViewer/components/ImageEmpty/ImageEmpty.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ImageEmpty.styles.css";
+import { ImageAbove } from "../../../../assets/icons/ImageAbove.icon";
+
+export const ImageEmpty = () => {
+  return (
+    <div className="image-empty--wrapper">
+      <ImageAbove />
+    </div>
+  );
+};
