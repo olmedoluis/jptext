9bc282fc8ccad259fcf107a8f2b8e3016b17ee3c
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 16:23:48 -0300

    refactor(TextBox.component.js):Hide content while loading

--- src/modules/FilesViewer/components/TextBox.component.js
+++ src/modules/FilesViewer/components/TextBox.component.js
@@ -3,12 +3,14 @@ import { Loader } from "../../../shared-components/Loader/Loader.component";
 import "./TextBox.styles.css";
 
 export const TextBox = ({ text, isLoading, boxRef = null }) => {
+  const value = text || " ";
+
   return (
     <div className="text-box--wrapper">
       <textarea
         className="text-box__input"
         ref={boxRef}
-        value={text || " "}
+        value={isLoading ? " " : value}
         onChange={() => {}}
       />
       {isLoading ? <Loader /> : ""}
