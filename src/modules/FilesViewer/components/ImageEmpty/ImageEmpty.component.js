701aca6d85bc4a28f2d2ebf888efc3364c77b4c7
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 18:49:43 -0300

    refactor(ImageEmpty.component.js):Add text below

--- src/modules/FilesViewer/components/ImageEmpty/ImageEmpty.component.js
+++ src/modules/FilesViewer/components/ImageEmpty/ImageEmpty.component.js
@@ -6,6 +6,8 @@ export const ImageEmpty = () => {
   return (
     <div className="image-empty--wrapper">
       <ImageAbove />
+      <h2>Upload an Image to start!</h2>
+      <p>Drop your images from above!</p>
     </div>
   );
 };
