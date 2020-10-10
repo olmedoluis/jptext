61473eeeef389f34f7b7733b80255f74faea0bee
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 15:45:02 -0300

    feat(Loader):Create loader component

---
+++ src/shared-components/Loader/Loader.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Loading } from "../../assets/icons/Loading.icon";
+
+export const Loader = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+};
