edac65fc157735b37690be4c8d15b3c03ff6c6b5
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 16:09:10 -0300

    feat(components):Create styles for text box

---
+++ src/modules/FilesViewer/components/TextBox.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Loader } from "../../shared-components/Loader/Loader.component";
+
+export const TextBox = ({ text, isLoading, boxRef = null }) => {
+  return (
+    <div className="text-box--wrapper">
+      <textarea
+        className="text-box__input"
+        ref={boxRef}
+        value={text || " "}
+        onChange={() => {}}
+      />
+      {isLoading ? <Loader /> : ""}
+    </div>
+  );
+};
