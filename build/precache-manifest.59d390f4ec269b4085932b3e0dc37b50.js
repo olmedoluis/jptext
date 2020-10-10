3a1ba6b8f6c46f6184edd66c1214f561a58e3fea
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 20:03:00 -0300

    :wq

---
+++ build/precache-manifest.59d390f4ec269b4085932b3e0dc37b50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ea5bb710fefb61964885cb78a52cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e193db929f7124cf635",
+    "url": "/static/css/main.1819b1c0.chunk.css"
+  },
+  {
+    "revision": "f2fcbe40c56946039b4e",
+    "url": "/static/js/2.0d36ff16.chunk.js"
+  },
+  {
+    "revision": "3e193db929f7124cf635",
+    "url": "/static/js/main.ab7e2602.chunk.js"
+  },
+  {
+    "revision": "41cff92cd76d0e85dbaf",
+    "url": "/static/js/runtime-main.2be8b6a7.js"
+  }
+]);
