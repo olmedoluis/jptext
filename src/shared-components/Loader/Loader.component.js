c95ea8f6f65661c55e7a55e0826de2ac50c51b2c
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 17:14:23 -0300

    feat(Loader.component.js):Expect for optional text to show below loader

--- src/shared-components/Loader/Loader.component.js
+++ src/shared-components/Loader/Loader.component.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Loading } from "../../assets/icons/Loading.icon";
-import "./Loader.styles.css"
+import "./Loader.styles.css";
 
-export const Loader = () => {
+export const Loader = ({ text }) => {
   return (
     <div className="loader--wrapper">
       <Loading />
+      {text && <p className="loader__text">{text}</p>}
     </div>
   );
 };
