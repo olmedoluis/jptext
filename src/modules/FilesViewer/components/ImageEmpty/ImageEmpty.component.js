464a19753cb23353f7d6e7366abff31db45a2453
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 19:50:06 -0300

    refactor(ImageEmpty):Use classes to append styles

--- src/modules/FilesViewer/components/ImageEmpty/ImageEmpty.component.js
+++ src/modules/FilesViewer/components/ImageEmpty/ImageEmpty.component.js
@@ -6,8 +6,8 @@ export const ImageEmpty = () => {
   return (
     <div className="image-empty--wrapper">
       <ImageAbove />
-      <h2>Upload an Image to start!</h2>
-      <p>Drop your images from above!</p>
+      <p className="image-empty__title">Upload an Image to start!</p>
+      <p className="image-empty__paragraph">Drop your images from above!</p>
     </div>
   );
 };
