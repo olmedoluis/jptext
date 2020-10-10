fdb32b0d2aea12654573f0071ba8073fee497347
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-10 19:30:53 -0300

    refactor(TextBox.component.js):Implement styles for text when there is no file

--- src/modules/FilesViewer/components/TextBox/TextBox.component.js
+++ src/modules/FilesViewer/components/TextBox/TextBox.component.js
@@ -3,17 +3,20 @@ import { Loader } from "../../../../shared-components/Loader/Loader.component";
 import "./TextBox.styles.css";
 
 export const TextBox = ({ text, isLoading, boxRef = null }) => {
-  const value = text || " ";
+  const value = isLoading || !text ? " " : text;
 
   return (
     <div className="text-box--wrapper">
       <textarea
         className="text-box__input"
         ref={boxRef}
-        value={isLoading ? " " : value}
+        value={value}
         onChange={() => {}}
       />
       {isLoading && <Loader text="Your file is loading!" />}
+      {!isLoading && !text && (
+        <p className="text-box__message">No images has been converted yet!</p>
+      )}
     </div>
   );
 };
